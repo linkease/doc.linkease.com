db094ac69d0f355d4f0b0edd90f277692716c287
xiangfeidexiaohuo <jsjson@163.com>
2026-07-03 16:34:57 +0800

    istoreos: 20260703
    Signed-off-by: xiangfeidexiaohuo <jsjson@163.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -284,6 +284,7 @@ module.exports = ctx => ({
                                 },
                                 {
                                     title: "更新日志", collapsable: true, children: [
+                                        ["changelog/20260703", "v24.10.7 - 20260703"],
                                         ["changelog/20260605", "v24.10.7 - 20260605"],
                                         ["changelog/20260509", "v24.10.6 - 20260509"],
                                         ["changelog/20260417", "v24.10.6 - 20260417"],
